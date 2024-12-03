f297521ad5e115345abda1624b0e71119be30808
Artem Starushyk <artem.starushyk@idealscorp.com>
2024-12-03 23:50:54 +0200

    upd

--- Hackathon.App/App.xaml.cs
+++ Hackathon.App/App.xaml.cs
@@ -1,21 +1,14 @@
-﻿using Hackathon.App.Services.Abstractions;
-
-namespace Hackathon.App;
+﻿namespace Hackathon.App;
 
 public partial class App : Application
 {
-    public static string BaseApiUrl = DeviceInfo.Platform == DevicePlatform.Android
+    public static readonly string BaseApiUrl = DeviceInfo.Platform == DevicePlatform.Android
         ? "http://10.0.2.2:5556" 
         : "http://localhost:5556";
     
     public App()
     {
         InitializeComponent();
-        //trayService.Initialize();
-        // trayService.ClickHandler = () =>
-        //     ServiceProvider.GetService<INotificationService>()
-        //         ?.ShowNotification("Hello Build! 😻 From .NET MAUI", "How's your weather?  It's sunny where we are 🌞");
-
     }
 
     protected override Window CreateWindow(IActivationState? activationState)
